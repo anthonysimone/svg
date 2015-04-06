80706da989831bdf9f7b3841696ca3fa119bd20d
Anthony Simone <anthony.simone7@gmail.com>
2015-04-05 21:11:05 -0600

    Add more to examples

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+
+
+/**
+ * Initialize all snap.svg toggles
+ */
+
 (function() {
     // initialize all
 
@@ -25,3 +31,25 @@
     //new svgIcon( document.querySelector( '.si-icons-hover .si-icon-maximize' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
     //new svgIcon( document.querySelector( '.si-icons-hover .si-icon-equalizer' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
 })();
+
+
+
+/**
+ * Add class to elements on click
+ */
+$('svg.shrink-click path, svg.shrink-click polygon, svg.shrink-click circle').on("click", function() {
+  $(this).attr("class", "shrink").delay(1000).queue(function() {
+    $(this).remove().dequeue();
+  });
+  //$(this).remove();
+  //alert('boom');
+});
+
+
+
+/**
+ * Stroke-dash array
+ */
+//var path = document.querySelector('.e3-path');
+//var pathLength = path.getTotalLength(); // 988.0062255859375
+//alert(pathLength);
